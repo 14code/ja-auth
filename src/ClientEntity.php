f22d375ec41dbea3b9cb58b4274a42934815e43c
David Hoppe <dh@smpmedia.net>
2020-03-14 06:49:15 +0100

    Refactor entity classes using traits

--- src/ClientEntity.php
+++ src/ClientEntity.php
@@ -1,34 +1,15 @@
 <?php
 declare(strict_types=1);
 
-
 namespace I4code\JaAuth;
 
-
 use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Entities\Traits\ClientTrait;
+use League\OAuth2\Server\Entities\Traits\EntityTrait;
 
 class ClientEntity implements ClientEntityInterface
 {
-
-    public function getIdentifier()
-    {
-        // TODO: Implement getIdentifier() method.
-    }
-
-    public function getName()
-    {
-        // TODO: Implement getName() method.
-    }
-
-    public function getRedirectUri()
-    {
-        $uri = 'notempty';
-        return $uri;
-    }
-
-    public function isConfidential()
-    {
-        // TODO: Implement isConfidential() method.
-    }
+    use EntityTrait;
+    use ClientTrait;
 
 }
--- src/UserEntity.php
+++ src/UserEntity.php
@@ -3,12 +3,10 @@ declare(strict_types=1);
 
 namespace I4code\JaAuth;
 
+use League\OAuth2\Server\Entities\Traits\EntityTrait;
 use League\OAuth2\Server\Entities\UserEntityInterface;
 
 class UserEntity implements UserEntityInterface
 {
-    public function getIdentifier()
-    {
-        // TODO: Implement getIdentifier() method.
-    }
+    use EntityTrait;
 }
