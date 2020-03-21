1239d9caaf47a8c3626960604eac11ca348a9c99
David Hoppe <dh@14co.de>
2020-03-21 07:42:19 +0100

    Refactor to improve abstraction of entity factories

---
+++ src/AbstractEntityFactory.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+
+namespace I4code\JaAuth;
+
+
+use I4code\JaApi\Factory;
+
+abstract class AbstractEntityFactory implements Factory
+{
+
+    public function createFromArray(array $data)
+    {
+        return $this->createFromObject((object) $data);
+    }
+
+    public function create($data = null)
+    {
+        if (is_array($data)) {
+            return $this->createFromArray($data);
+        }
+        if (is_object($data)) {
+            return $this->createFromObject($data);
+        }
+    }
+
+    abstract public function createFromObject(object $data);
+
+}
--- src/ClientEntityFactory.php
+++ src/ClientEntityFactory.php
@@ -5,25 +5,8 @@ declare(strict_types=1);
 namespace I4code\JaAuth;
 
 
-use I4code\JaApi\Factory;
-
-class ClientEntityFactory implements Factory
+class ClientEntityFactory extends AbstractEntityFactory
 {
-
-    public function createFromArray(array $data)
-    {
-        return $this->createFromObject((object) $data);
-    }
-
-    public function create($data = null)
-    {
-        if (is_array($data)) {
-            return $this->createFromArray($data);
-        }
-        if (is_object($data)) {
-            return $this->createFromObject($data);
-        }
-    }
 
     public function createFromObject(object $data)
     {
